4837c91907922abbf7771efc9ee97fad9a8192e7
chrisgrounds <iamchrisgrounds@gmail.com>
2026-09-02 14:48:14 +0100

    init arena, working in a small set of cases

---
+++ README.md
@@ -0,0 +1,21 @@
+# Arena
+
+_This is an LLM free zone_
+
+This is a byte-arena allocator for learning.
+
+An arena is an API which pre-allocates a block of memory and allows the user to sequentially fill up the memory by moving a pointer forward when allocating new memory.
+
+This arena is configurable on number of chunks and has the following API:
+
+```rust
+fn Arena::new(num_chunks: usize) -> Arena;
+fn Arena::allocate(self, item: u8) -> *mut u8;
+impl Drop for Arena;
+```
+
+`Arena::new` creates a new arena with `num_chunks` number of chunks. `Arena::allocate` allocates a byte into the arena and returns the pointer to that chunk. And dropping the arena interally calls the `Arena::deallocate` private function.
+
+## Caveats / TODO
+
+At the moment this arena is hardcoded to storing single bytes and does not take different types or sizes of objects to write to memory. It also only allows configuration of "number of chunks" which are all fixed-size. And even worse, once the user goes over the arena size, the `Arena::allocate` function simply returns `None` and does not create a new arena. Instead the arena should pre-allocate memory, allow different size "chunks", and create a new arena once the user goes over the size of the existing arena.
---
+++ src/lib.rs
@@ -0,0 +1,135 @@
+type Pointer = *mut u8;
+
+type ChunkSize = u8;
+
+pub struct Arena {
+  start: Pointer,
+  end: Pointer,
+  pub current_ptr_pos: Pointer,
+  layout: std::alloc::Layout,
+}
+
+impl Arena {
+  pub fn new(num_chunks: usize) -> Self {
+    unsafe {
+      let layout = std::alloc::Layout::array::<ChunkSize>(num_chunks).unwrap();
+      let start = std::alloc::alloc(layout);
+
+      let current_ptr_pos = start.clone();
+      let end = start.add(num_chunks);
+
+      Self {
+        start,
+        end,
+        current_ptr_pos,
+        layout,
+      }
+    }
+  }
+
+  pub fn allocate(&mut self, item: u8) -> Option<Pointer> {
+    match self.current_ptr_pos >= self.end {
+      true => None,
+      false => unsafe {
+        let current_ptr = self.current_ptr_pos;
+        core::ptr::write(current_ptr, item);
+
+        self.current_ptr_pos = current_ptr.add(1);
+
+        Some(current_ptr)
+      },
+    }
+  }
+}
+
+impl Drop for Arena {
+  fn drop(&mut self) {
+    unsafe {
+      std::alloc::dealloc(self.start, self.layout);
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use quickcheck::quickcheck;
+  use std::assert_eq;
+
+  use super::*;
+
+  #[test]
+  fn no_ops_on_zero_chunk_size() {
+    let arena = Arena::new(0);
+
+    assert_eq!(arena.start, arena.end);
+    assert_eq!(arena.start, arena.current_ptr_pos);
+    assert_eq!(arena.end, arena.current_ptr_pos);
+  }
+
+  quickcheck! {
+    fn prop_correctly_offsets_arena_end(num_chunks: usize) -> bool {
+      let arena = Arena::new(num_chunks);
+      unsafe {
+        let expected_end = arena.start.add(num_chunks);
+
+        arena.end == expected_end
+      }
+    }
+  }
+
+  #[test]
+  fn allocation_returns_correct_pointer_at_start() {
+    let mut arena = Arena::new(1);
+
+    let ptr = arena.allocate(12);
+
+    assert_eq!(Some(arena.start), ptr);
+  }
+
+  #[test]
+  fn can_allocate_multiple_chunks() {
+    let mut arena = Arena::new(3);
+
+    let ptr1 = arena.allocate(1).unwrap();
+    let ptr2 = arena.allocate(2).unwrap();
+    let ptr3 = arena.allocate(3).unwrap();
+
+    unsafe {
+      println!("start ptr: {:?}", arena.start);
+      println!(
+        "[chunk 1] ptr: {:?}, content: {:?}",
+        ptr1,
+        std::ptr::read(ptr1)
+      );
+      println!(
+        "[chunk 2] ptr: {:?}, content: {:?}",
+        ptr2,
+        std::ptr::read(ptr2)
+      );
+      println!(
+        "[chunk 3] ptr: {:?}, content: {:?}",
+        ptr3,
+        std::ptr::read(ptr3)
+      );
+      println!("end ptr: {:?}", arena.end);
+    }
+
+    unsafe {
+      assert_eq!(1, std::ptr::read(ptr1));
+      assert_eq!(2, std::ptr::read(ptr2));
+      assert_eq!(3, std::ptr::read(ptr3));
+    }
+  }
+
+  #[test]
+  fn cannot_allocate_more_than_block_size() {
+    let mut arena = Arena::new(3);
+
+    let _ = arena.allocate(1);
+    let _ = arena.allocate(2);
+    let _ = arena.allocate(3);
+    let ptr4 = arena.allocate(4);
+
+    assert_eq!(None, ptr4);
+  }
+}
